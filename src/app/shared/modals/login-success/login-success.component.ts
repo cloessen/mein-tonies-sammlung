ccfbc967177fef2498bb6f3ead09ed55276ab5e3
cloessen <s.claussen@me.com>
2018-09-13 10:12:25 +0200

    safing progress, switching machines

--- src/app/shared/modals/login-success/login-success.component.ts
+++ src/app/shared/modals/login-success/login-success.component.ts
@@ -3,15 +3,15 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  templateUrl: './login-success.component.html',
+  styleUrls: ['./login-success.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginSuccessComponent implements OnInit {
 
   constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit() {
-    setTimeout(this.activeModal.close, 3000);
+    setTimeout(this.activeModal.close, 1500);
   }
   onCloseClick() {
     this.activeModal.close();
